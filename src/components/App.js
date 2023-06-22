113321cb2ed473b44bb3027cc3efd2fef090b89f
thilaks-hash <thilagavathyr025@gmail.com>
2023-06-22 14:30:47 +0530

    updated

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,24 @@
-import React from 'react'
-import '../styles/App.css';
+import React from "react";
+import "../styles/App.css";
 
 class App extends React.Component {
-  
+  constructor(props) {
+    super(props);
+    this.state = { name: "John Doe", enrollmentNo: "12345678", age: 34 };
+  }
+  Increaseage = () => {
+    this.setState({ age: this.state.age + 1 });
+  };
+  render() {
+    return (
+      <>
+        <h1>Hello, my name is {this.state.name}</h1>
+        <p>I am {this.state.age} years old`</p>
+        <p>my enrollment no is{this.state.enrollmentNo}</p>
+        <button onClick={this.Increaseage}>Increase age</button>
+      </>
+    );
+  }
 }
-
 
 export default App;
