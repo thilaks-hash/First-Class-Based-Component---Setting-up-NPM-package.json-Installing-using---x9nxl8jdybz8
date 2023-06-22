0699eda1a1a15cad1ab3026bf152ffb2c8fe07a5
thilaks-hash <thilagavathyr025@gmail.com>
2023-06-22 14:50:02 +0530

    updated

--- src/components/App.js
+++ src/components/App.js
@@ -14,7 +14,7 @@ class App extends React.Component {
       <>
         <h1>Hello, my name is {this.state.name}</h1>
         <p>
-          I am {this.state.age} years old and my enrollment no is
+          I am {this.state.age} years old and my enrollment no is{" "}
           {this.state.enrollmentNo}
         </p>
         <button onClick={this.Increaseage}>Increase age</button>
